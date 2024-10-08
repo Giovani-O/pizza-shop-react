db9c0fd2c587621e59129241300ea2d91531282b
Giovani <giovani98@proton.me>
2024-10-08 20:49:38 -0300

    chore: prettier settings

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -10,17 +10,23 @@ export function SignIn() {
       <div className="p-8">
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className='text-2xl font-semibold tracking-tight'>Acessar painel</h1>
-            <span className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro</span>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <span className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </span>
           </div>
 
           <form className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email"/>
+              <Input id="email" type="email" />
             </div>
 
-            <Button className="w-full" type="submit">Acessar painel</Button> 
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
