bac5dcc0619fb07cf48b402370f03d81ec86adcf
Giovani <giovani98@proton.me>
2024-10-09 13:30:47 -0300

    feat: Finish application header

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,16 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import './global.css'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
